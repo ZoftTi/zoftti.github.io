df7718d19213699a0a305bd3c4230d672f51f081
ZoftTi <poleorder@icloud.com>
2022-06-07 15:36:18 +0800

    🐳 chore: update theme-reco

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,90 +1,25 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
+import recoTheme from 'vuepress-theme-reco'
 
-export default defineUserConfig<DefaultThemeOptions>({
+export default defineUserConfig({
   title: '柒橙的博客',
   description: '',
-  dest: 'public',
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }],
-    [
-      'meta',
-      {
-        name: 'viewport',
-        content: 'width=device-width,initial-scale=1,user-scalable=no',
-      },
-    ],
-  ],
-  theme: 'reco',
-  lang: 'zh-CN',
-  themeConfig: {
+  theme: recoTheme({
     style: '@vuepress-reco/style-default',
     logo: '/head.jpg',
-    author: '柒橙',
+    author: 'reco_luan',
     authorAvatar: '/head.jpg',
     docsRepo: 'https://github.com/vuepress-reco/vuepress-theme-reco-next',
     docsBranch: 'main',
     docsDir: 'example',
     lastUpdatedText: '',
-    navbar: [
+    navbar:
+    [
       { text: 'Home', link: '/' },
       { text: 'Categories', link: '/categories/reco/1/' },
-      { text: 'Tags', link: '/tags/reco/1' },
+      { text: 'Tags', link: '/tags/reco/1/' },
     ],
-    // bulletin: {
-    //   body: [
-    //     {
-    //       type: 'text',
-    //       content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-    //       style: 'font-size: 12px;',
-    //     },
-    //     {
-    //       type: 'hr',
-    //     },
-    //     {
-    //       type: 'title',
-    //       content: 'QQ 群',
-    //     },
-    //     {
-    //       type: 'text',
-    //       content: `
-    //       <ul>
-    //         <li>QQ群1：1037296104</li>
-    //         <li>QQ群2：1061561395</li>
-    //         <li>QQ群3：962687802</li>
-    //       </ul>`,
-    //       style: 'font-size: 12px;',
-    //     },
-    //     {
-    //       type: 'hr',
-    //     },
-    //     {
-    //       type: 'title',
-    //       content: 'GitHub',
-    //     },
-    //     {
-    //       type: 'text',
-    //       content: `
-    //       <ul>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-    //         <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-    //       </ul>`,
-    //       style: 'font-size: 12px;',
-    //     },
-    //     {
-    //       type: 'hr',
-    //     },
-    //     {
-    //       type: 'buttongroup',
-    //       children: [
-    //         {
-    //           text: '打赏',
-    //           link: '/docs/others/donate.html',
-    //         },
-    //       ],
-    //     },
-    //   ],
-    // },
     // valineConfig 配置与 1.x 一致
     // valineConfig: {
     //   appId: 'xxx',
@@ -95,6 +30,6 @@ export default defineUserConfig<DefaultThemeOptions>({
     //   recordIP: true,
     //   // hideComments: true // 隐藏评论
     // },
-  },
+  }),
   // debug: true,
 })
